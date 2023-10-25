501f01f041b9140a35dab5984f0e760ea95b07f6
DenysKhrushch <denis.hrusch@gmail.com>
2023-10-25 07:02:43 +0200

    updated cors allowed origins

--- apps/express-server/src/middleware/corsHandler.ts
+++ apps/express-server/src/middleware/corsHandler.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 import { Application } from 'express';
 
 export function corsHandler(app: Application) {
-  const allowedOrigins = ['http://example.com', 'http://localhost:4200']; // Replace with your allowed origins
+  const allowedOrigins = ['https://mini-blog-teal-rho.vercel.app', 'http://localhost:4200']; // Replace with your allowed origins
 
   const corsOptions = {
     origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
