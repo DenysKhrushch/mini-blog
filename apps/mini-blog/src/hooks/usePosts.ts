096b9f48db9bf55d4fc8bf84b7b94c6605b582ac
DenysKhrushch <denis.hrusch@gmail.com>
2023-10-25 07:17:07 +0200

    updated API url

--- apps/mini-blog/src/hooks/usePosts.ts
+++ apps/mini-blog/src/hooks/usePosts.ts
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import {fetcher} from "../api";
 
 export const usePosts = () => {
-  const { data, error, isLoading } = useSWR(`mini-blog-server.vercel.app/posts`, fetcher)
+  const { data, error, isLoading } = useSWR(`https://mini-blog-server-git-main-denyskhrushch.vercel.app/posts`, fetcher)
 
   return {
     posts: data,
